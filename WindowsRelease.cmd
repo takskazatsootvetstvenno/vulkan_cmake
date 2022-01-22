761ad1102c0e6846df50545dc627c9faf1e10e03
Titaev Denis <dtitaev@vrconcept.net>
2022-01-22 14:15:56 +0300

    fix project dir name

--- WindowsRelease.cmd
+++ WindowsRelease.cmd
@@ -2,8 +2,8 @@ cd ..
 if exist build rmdir /s /q build
 if exist install rmdir /s /q install
 mkdir build
-conan install VulkanEngine -if build --build missing -s build_type=Release -r conancenter
+conan install vulkan_cmake -if build --build missing -s build_type=Release -r conancenter
 cd build
-cmake ../VulkanEngine -G "Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE=Release
+cmake ../vulkan_cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release
 cmake --install .
