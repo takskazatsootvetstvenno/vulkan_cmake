fccf27146780feb12e75e813152ce359cb626707
Titaev Denis <dtitaev@vrconcept.net>
2022-02-07 12:06:08 +0300

    fix building

--- WindowsRelease.cmd
+++ WindowsRelease.cmd
@@ -2,7 +2,7 @@ cd ..
 if exist build rmdir /s /q build
 if exist install rmdir /s /q install
 mkdir build
-conan install vulkan_cmake -if build --build missing -s build_type=Release -s compiler="Visual Studio" -r conancenter
+conan install vulkan_cmake -if build --build missing -s build_type=Release -s compiler="Visual Studio" -s compiler.version=16 -s compiler.runtime="MDd" -r conancenter
 cd build
 cmake ../vulkan_cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release
