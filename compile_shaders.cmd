456b01db992fe6ab3fd88394248bfd3fa600f666
Denis <dtitaev@vrconcept.net>
2022-03-12 14:01:29 +0300

    fix shader compile rule for Windows

--- compile_shaders.cmd
+++ compile_shaders.cmd
@@ -1,2 +1,2 @@
-glslc.exe data\shaders\vertex.vert -o data\shaders\vertex.vert.spv
-glslc.exe data\shaders\fragment.frag -o data\shaders\fragment.frag.spv
+if exist glslc.exe glslc.exe data\shaders\vertex.vert -o data\shaders\vertex.vert.spv
+if exist glslc.exe glslc.exe data\shaders\fragment.frag -o data\shaders\fragment.frag.spv
